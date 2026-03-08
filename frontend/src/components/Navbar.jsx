29b577f62a9ba773d9f3d4c96cf198dd834a62f5
yamini777 <uppalyamini2004@gmail.com>
2026-03-08 14:58:28 +0530

    added changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-<h1>Charan</h1>
-        <img src={assets.logo} className= 'w-36' alt=""/>
+      <img src={assets.logo} className= 'w-36' alt=""/>
     </div>
   )
 }
