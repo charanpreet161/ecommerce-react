80a8c46febbb452ff1e533165931ab2eca2faebe
charanpreet161 <cpkour16122000@gmail.com>
2026-04-02 21:35:37 +0530

    problem in footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import LatestCollection from "../components/LatestCollection";
+import Hero from "../components/Hero";
+import BestSeller from '../components/BestSeller';
+import OurPolicy from '../components/OurPolicy';
+import NewsletterBox from '../components/NewsletterBox';
 const Home = () => {
   return (
     <div>
-      
+      <Hero />
+      <LatestCollection />
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsletterBox/>
     </div>
   )
 }
