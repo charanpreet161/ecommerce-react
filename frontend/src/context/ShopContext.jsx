127267de88cbf8882e16082591a883a880e4999f
charanpreet161 <cpkour16122000@gmail.com>
2026-04-03 20:13:57 +0530

    added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { products } from "../assets/assets";
+
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
 
-    const currency = '$';
-    const delivery_fee = 10;
+    const currency = 'Rs. ';
+    const delivery_fee = 50;
 
+    const [search,setSearch] = useState('');
+    const [showSearch,setShowSearch] = useState(true);
 
 const value = {
-products , currency , delivery_fee
-}
+products , currency , delivery_fee, 
+search, setSearch, showSearch, setShowSearch
+};
 
 return (
 <ShopContext.Provider value={value}>
 {props.children}
 </ShopContext.Provider>
-)
+);
 
-}
+};
 
 export default ShopContextProvider
