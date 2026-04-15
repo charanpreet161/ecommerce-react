455c604afd44691f98f1fb00fcbeb67b2b1093fd
yamini777 <uppalyamini2004@gmail.com>
2026-04-15 12:07:31 +0530

    added admin folder

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -60,11 +60,28 @@ const listProducts = async (req, res) => {
 
 // function for removing product
 const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message: "Product Removed"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 // function for single product info
 const singleProduct = async (req, res) => {
+    try {
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
