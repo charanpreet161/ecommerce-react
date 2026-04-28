77ad7ea71c38fd529d75fe2d5cf8c91a2b407ecf
yamini777 <uppalyamini2004@gmail.com>
2026-04-28 14:55:38 +0530

    added changes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,14 @@
 import orderModel from "../models/orderModal.js";
 import userModel from "../models/userModel.js";
+import Stripe from 'stripe'
+
+//global variable
+const currency='inr'
+const deliveryCharge=50
 
 
+// gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing order using cod method
 const placeOrder = async (req, res) => {
@@ -35,7 +42,57 @@ const placeOrder = async (req, res) => {
 
 // Placing orders using Stripe Method
 const placeOrderStripe = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body;
+        const { origin } = req.headers;
 
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: 'Delivery Charges'
+                },
+                unit_amount: deliveryCharge * 100
+            },
+            quantity: 1
+        })
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment',
+        })
+
+        res.json({success:true,session_url:session.url});
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
 }
 
 // Placing orders using Razorpay Method
@@ -45,6 +102,15 @@ const placeOrderRazorpay = async (req, res) => {
 
 // All Orders data for Admin Panel
 const allOrders = async (req, res) => {
+    try {
+
+        const orders = await orderModel.find({})
+        res.json({ success: true, orders })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
@@ -66,6 +132,17 @@ const userOrders = async (req, res) => {
 
 // update order status from Admin Panel
 const updateStatus = async (req, res) => {
+    try {
+
+        const { orderId, status } = req.body
+
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({ success: true, message: 'Status Updated' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
