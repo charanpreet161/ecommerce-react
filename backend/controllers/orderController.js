7bfb20992b39ebc06002aa16ebab2302aec1daf5
yamini777 <uppalyamini2004@gmail.com>
2026-05-11 18:22:28 +0530

    added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 import orderModel from "../models/orderModal.js";
 import userModel from "../models/userModel.js";
 import Stripe from 'stripe'
-import razorpay from 'razorpay'
+//import razorpay from 'razorpay'
 
 //global variable
 const currency='inr'
@@ -10,10 +10,10 @@ const deliveryCharge=50
 
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-const razorpayInstance = new razorpay({
+/*const razorpayInstance = new razorpay({
     key_id : process.env.RAZORPAY_KEY_ID,
     key_secret : process.env.RAZORPAY_KEY_SECRET,
-})
+})*/
 
 //placing order using cod method
 const placeOrder = async (req, res) => {
@@ -119,7 +119,7 @@ const verifyStripe = async (req, res) => {
 }
 
 // Placing orders using Razorpay Method
-const placeOrderRazorpay = async (req, res) => {
+/*const placeOrderRazorpay = async (req, res) => {
     try {
         const { userId, items, amount, address } = req.body;
 
@@ -153,26 +153,26 @@ const placeOrderRazorpay = async (req, res) => {
         console.log(error)
         res.json({success:false,message:error.message})
     }
-}
+}*/
 
-const verifyRazorpay = async (req, res) => {
-    try {
-        const { userId, razorpay_order_id } = req.body
+// const verifyRazorpay = async (req, res) => {
+//     try {
+//         const { userId, razorpay_order_id } = req.body
 
-        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
-        if (orderInfo.status === 'paid') {
-            await orderModel.findByIdAndUpdate(orderInfo.receipt, { payment: true });
-            await userModel.findByIdAndUpdate(userId, { cartData: {} })
-            res.json({ success: true, message: "Payment Successful" })
-        } else {
-            res.json({ success: false, message: 'Payment Failed' });
-        }
+//         const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+//         if (orderInfo.status === 'paid') {
+//             await orderModel.findByIdAndUpdate(orderInfo.receipt, { payment: true });
+//             await userModel.findByIdAndUpdate(userId, { cartData: {} })
+//             res.json({ success: true, message: "Payment Successful" })
+//         } else {
+//             res.json({ success: false, message: 'Payment Failed' });
+//         }
 
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
 
 
 // All Orders data for Admin Panel
@@ -221,4 +221,4 @@ const updateStatus = async (req, res) => {
 
 }
 
-export {verifyRazorpay, verifyStripe, placeOrder, placeOrderStripe,placeOrderRazorpay,allOrders,userOrders,updateStatus}
+export {  placeOrder, placeOrderStripe,allOrders,userOrders,updateStatus}
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -29,33 +29,33 @@ const PlaceOrder = () => {
     setFormData(data => ({ ...data, [name]: value }))
   }
 
-  const initPay = (order) => {
-    const options = {
-      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
-      amount: order.amount,
-      currency: order.currency,
-      name: 'Order Payment',
-      description: 'Order Payment',
-      order_id: order.id,
-      receipt: order.receipt,
-      handler: async (response) => {
-        console.log(response)
-        try {
-          const { data } = await axios.post(backendUrl + '/api/order/verifyRazorpay', response, { headers: { token } })
-          if (data.success) {
-            navigate('/orders')
-            setCartItems({})
-          }
-        } catch (error) {
-          console.log(error)
-          toast.error(error.message)
-        }
+  // const initPay = (order) => {
+  //   const options = {
+  //     key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+  //     amount: order.amount,
+  //     currency: order.currency,
+  //     name: 'Order Payment',
+  //     description: 'Order Payment',
+  //     order_id: order.id,
+  //     receipt: order.receipt,
+  //     handler: async (response) => {
+  //       console.log(response)
+  //       try {
+  //         const { data } = await axios.post(backendUrl + '/api/order/verifyRazorpay', response, { headers: { token } })
+  //         if (data.success) {
+  //           navigate('/orders')
+  //           setCartItems({})
+  //         }
+  //       } catch (error) {
+  //         console.log(error)
+  //         toast.error(error.message)
+  //       }
       
-      }
-    }
-    const rzp = new window.Razorpay(options)
-    rzp.open()
-  }
+  //     }
+  //   }
+  //   const rzp = new window.Razorpay(options)
+  //   rzp.open()
+  // }
 
 
   const onSubmitHandler = async (event) => {
@@ -103,15 +103,15 @@ const PlaceOrder = () => {
             toast.error(responseStripe.data.message)
           }
           break;
-        case 'razorpay':
+        // case 'razorpay':
 
-           const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData, { headers: { token } })
-          if (responseRazorpay.data.success) {
-            initPay(responseRazorpay.data.order);
-          }
+        //    const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData, { headers: { token } })
+        //   if (responseRazorpay.data.success) {
+        //     initPay(responseRazorpay.data.order);
+        //   }
 
 
-          break;
+        //   break;
 
 
         default:
@@ -166,10 +166,10 @@ const PlaceOrder = () => {
               <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
           </div>
           
-            <div onClick={()=>setMethod('razorpay')}  className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+            {/* <div onClick={()=>setMethod('razorpay')}  className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
-          </div>
+          </div> */}
           
             <div onClick={()=>setMethod('cod')}  className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
